0767f2ad6f99cafccbd7fdf5b5c1626a6edad4f6
IrishJohnJacinto <irishjohnjacinto@gmail.com>
2026-01-31 20:05:14 +0800

    removed field

--- database/migrations/2025_11_19_072913_create_bookings_table.php
+++ database/migrations/2025_11_19_072913_create_bookings_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->decimal('totalPrice', 10, 2);
             $table->enum('bookingStatus', ['pending', 'confirmed', 'cancelled', 'completed'])->default('pending');
             $table->string('gcash_payment_intent_id')->nullable();
-            $table->enum('paymentStatus', ['unpaid', 'partial', 'paid'])->default('unpaid');
+           // $table->enum('paymentStatus', ['unpaid', 'partial', 'paid'])->default('unpaid');
             $table->enum('bookingType', ['day-use', 'overnight', 'special-event']);
             $table->enum('eventType', ['normal-booking', 'birthday', 'anniversary', 'wedding', 'corporate', 'christening','reunion','other'])->default('normal-booking');
             $table->text('specialRequirements')->nullable();
