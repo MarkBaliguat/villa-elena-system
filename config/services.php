d3c3d19975b06cdc686c5d54462a43ab2213b8d9
IrishJohnJacinto <irishjohnjacinto@gmail.com>
2026-02-06 10:06:13 +0800

    change images name

--- config/services.php
+++ config/services.php
@@ -38,5 +38,6 @@ return [
     'paymongo' => [
     'secret_key' => env('PAYMONGO_SECRET_KEY'),
     'public_key' => env('PAYMONGO_PUBLIC_KEY'),
+    'api_url' => env('PAYMONGO_API_URL', 'https://api.paymongo.com/v1'),
 ],
 ];
