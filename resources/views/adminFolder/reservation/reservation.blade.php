20103cf10d90c1aaad444e1ebae6e1923f6b6890
IrishJohnJacinto <irishjohnjacinto@gmail.com>
2026-03-31 13:41:47 +0800

    removed ko na delete button baguhin nalang ang booking status para mapunta sa history i mark as cancelled nalang ganon

--- resources/views/adminFolder/reservation/reservation.blade.php
+++ resources/views/adminFolder/reservation/reservation.blade.php
@@ -713,11 +713,13 @@
                             <button onclick="openPaymentModal(${booking.bookingID})" class="text-green-600 hover:text-green-800" title="Payment Management">
                                 <i class="fas fa-credit-card"></i>
                             </button>
+                            <!--
                             @if(auth()->user()->role === 'manager')
                                 <button onclick="deleteBooking(${booking.bookingID})" class="text-red-600 hover:text-red-800" title="Delete">
                                     <i class="fas fa-trash"></i>
                                 </button>
                             @endif
+                            -->
                         </div>
                     </td>
                 </tr>
@@ -807,9 +809,14 @@
                         <button class="btn-card-payment" onclick="openPaymentModal(${booking.bookingID})">
                             <i class="fas fa-credit-card"></i> Payment
                         </button>
+                        <!--
+                        @if(auth()->user()->role === 'manager')
                         <button class="btn-card-delete" onclick="deleteBooking(${booking.bookingID})">
                             <i class="fas fa-trash"></i> Delete
                         </button>
+                        @endif
+                        -->
+
                     </div>
                 </div>
                 `;
